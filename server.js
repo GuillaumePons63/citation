7e71a4e2b4cb0ff2effcdb7986bebd36e9d9f783
Guillaumeweb <guillaume@web-63.fr>
2022-06-05 10:27:46 +0200

    setup express et server

---
+++ server.js
@@ -0,0 +1,18 @@
+if (typeof PhusionPassenger !== "undefined") {
+  PhusionPassenger.configure({ autoInstall: false });
+}
+
+var express = require("express");
+var app = express();
+app.get("/", function (req, res) {
+  var body = "Hello World";
+  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Length", body.length);
+  res.end(body);
+});
+
+if (typeof PhusionPassenger !== "undefined") {
+  app.listen("passenger");
+} else {
+  app.listen(3000);
+}
