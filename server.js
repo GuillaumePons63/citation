b5f64092d26cab96df164954b4e27de87217492a
Guillaumeweb <guillaume@web-63.fr>
2022-06-25 11:39:31 +0200

    setup .env

--- server.js
+++ server.js
@@ -1,18 +1,52 @@
-if (typeof PhusionPassenger !== "undefined") {
-  PhusionPassenger.configure({ autoInstall: false });
-}
+//Déclaration des constantes
+const http = require("http");
+const app = require("./app");
 
-var express = require("express");
-var app = express();
-app.get("/", function (req, res) {
-  var body = "Hello World";
-  res.setHeader("Content-Type", "text/plain");
-  res.setHeader("Content-Length", body.length);
-  res.end(body);
+// fonction necessaire au fonctionnement du serveur
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+// Gestion des erreurs
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+// Relation avec le fichier app.js
+const server = http.createServer(app);
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
 });
 
-if (typeof PhusionPassenger !== "undefined") {
-  app.listen("passenger");
-} else {
-  app.listen(3000);
-}
+server.listen();
